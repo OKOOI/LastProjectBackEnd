0b0c7ad9b23929349b1688b8c058a32067517704
OKOOI <jyslovly1993@gmail.com>
2023-05-12 18:06:00 +0900

    2023.05.12

--- src/main/java/project/mapper/ProjectMapper.java
+++ src/main/java/project/mapper/ProjectMapper.java
@@ -5,26 +5,55 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import project.dto.NoticeDto;
-
+import project.dto.QnaCommentDto;
+import project.dto.QnaDto;
 
 @Mapper
 public interface ProjectMapper {
 
 	// 공지사항 리스트 조회
 	public List<NoticeDto> listNoticeDto() throws Exception;
-	
+
 	// 공지사항 글별 상세페이지 조회
 	public NoticeDto noticeDetail(int noticeIdx) throws Exception;
 
 	// 공지사항 작성
 	public int insertNotice(NoticeDto noticeDto) throws Exception;
-	
+
 	// 공지사항 삭제
 	public int deleteNotice(int noticeIdx) throws Exception;
 
 	// 공지사항 수정
 	public int updateNotice(NoticeDto noticeDto) throws Exception;
 
+	// QNA 리스트 조회
+	public List<QnaDto> listQnaDto() throws Exception;
 
+	// QNA 상세페이지 조회
+//	public QnaDto qnaDetail(int qnaIdx) throws Exception;
+
+	// QNA 작성
+	public int insertQna(QnaDto qnaDto) throws Exception;
+
+	// QNA 삭제
+	public int deleteQna(int qnaIdx) throws Exception;
+
+	// QNA 수정
+	public int updateQna(QnaDto qnaDto) throws Exception;
+	
+	// QNA 페이지별 조회
+	public List<QnaDto> listQnaDtoByPages(int pages) throws Exception;
+	
+	// QNA 페이지수 조회
+	public int listQnaDtoPageCount() throws Exception;
+
+	// QNA 상세페이지 COMMENT 리스트
+	public List<QnaCommentDto> selectCommentList(int qnaIdx) throws Exception;
+	
+	// QNA 상세페이지 조회
+	public QnaDto selectQnaInfo(int qnaIdx) throws Exception;
+
+	// QNA 상세페이지 COMMENT 등록
+	public int insertComment(QnaCommentDto qnaCommentDto) throws Exception;
 
 }
--- src/main/java/project/service/ProjectService.java
+++ src/main/java/project/service/ProjectService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import project.dto.NoticeDto;
+import project.dto.QnaCommentDto;
+import project.dto.QnaDto;
 import project.mapper.ProjectMapper;
 
 @Service
@@ -29,7 +31,7 @@ public class ProjectService {
 	public int insertNotice(NoticeDto noticeDto) throws Exception {
 		return mapper.insertNotice(noticeDto);
 	}
-	
+
 	// 공지사항 삭제
 	public int deleteNotice(int noticeIdx) throws Exception {
 		return mapper.deleteNotice(noticeIdx);
@@ -40,4 +42,56 @@ public class ProjectService {
 		return mapper.updateNotice(noticeDto);
 	}
 
+	// QNA 리스트 조회
+	public List<QnaDto> listQnaDto() throws Exception {
+		return mapper.listQnaDto();
+	}
+
+	// QNA 상세페이지 조회
+//	public QnaDto qnaDetail(int qnaIdx) throws Exception {
+//		return mapper.qnaDetail(qnaIdx);
+//	}
+
+	// QNA 작성
+	public int insertQna(QnaDto qnaDto) throws Exception {
+		return mapper.insertQna(qnaDto);
+	}
+
+	// QNA 삭제
+	public int deleteQna(int qnaIdx) throws Exception {
+		return mapper.deleteQna(qnaIdx);
+	}
+
+	// QNA 수정
+	public int updateQna(QnaDto qnaDto) throws Exception {
+		return mapper.updateQna(qnaDto);
+	}
+
+	// QNA 페이지별 조회
+	public List<QnaDto> listQnaDtoByPages(int pages) throws Exception {
+
+		int offsetStart = (pages - 1) * 10;
+
+		return mapper.listQnaDtoByPages(offsetStart);
+	}
+
+	// QNA 페이지수 조회
+	public int listQnaDtoPageCount() throws Exception {
+		return mapper.listQnaDtoPageCount();
+	}
+
+	// QNA 상세페이지 COMMENT 리스트
+	public List<QnaCommentDto> selectCommentList(int qnaIdx) throws Exception {
+		return mapper.selectCommentList(qnaIdx);
+	}
+	
+	// QNA 상세페이지 조회
+	public QnaDto selectQnaInfo(int qnaIdx) throws Exception {
+		return mapper.selectQnaInfo(qnaIdx);
+	}
+	
+	// QNA 상세페이지 COMMENT 등록
+	public int insertComment(QnaCommentDto qnaCommentDto) throws Exception {
+		return mapper.insertComment(qnaCommentDto);
+	}
 }
